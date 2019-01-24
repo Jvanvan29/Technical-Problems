5660030cd7dd758c60fe2fbefce66fe3aab8b8d4
Jvanvan29 <46982944+Jvanvan29@users.noreply.github.com>
2019-01-23 21:17:03 -0500

    Add files via upload

---
+++ Problem1.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package technicalproblems;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jay's PC
+ */
+public class Problem1 {
+    
+    int sum;
+    int[] finArray; 
+    int firstNum;
+    int secondNum;
+    
+    Scanner input = new Scanner(System.in);
+    
+    int sizeOfArray = 0 ;
+    int pairSum = 0;
+    
+    public void arraySize() {
+           
+           System.out.println("What size array would you like?");
+           sizeOfArray = input.nextInt();
+        
+           int[] arr = new int[sizeOfArray];
+           finArray = arr;
+           
+    }//End makeArray method
+    
+    public void setArrayData() {
+        
+           for(int i = 0; i < finArray.length; i++ ) {
+               
+               System.out.println("Please enter a integer for this array?");
+               finArray[i] = input.nextInt();
+               
+           }//End for loop for putting data into array
+        
+    }
+    
+    public void sumOfPair() {
+        System.out.println("What sum would you like the pairs to add up too?");
+        pairSum = input.nextInt();
+        sum = pairSum;
+        
+    }//End sumOfPair method
+    
+    public void findPairsOfArray() {
+        
+        for(int i = 0; i < finArray.length; i++) {
+            
+            firstNum = finArray[i];
+            
+            for(int j = i+1; j < finArray.length; j++) {
+                
+                if(Math.abs(sum - finArray[i]) == finArray[j])
+                {
+                    secondNum = finArray[j];
+                    System.out.println(firstNum + " + " + secondNum +" = "+ sum );
+                    
+                }//End if statement
+                
+            }//End trying to find matching sum 
+            
+        }//End finArray length loop
+        
+    }//End findPairsOfArray
+    
+}
---
+++ TechnicalProblems.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package technicalproblems;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jay's PC
+ */
+public class TechnicalProblems {
+    
+    int sum;
+    int[] finArray; 
+    int firstNum;
+    int secondNum;
+    
+    Scanner input = new Scanner(System.in);
+    
+    int sizeOfArray = 0 ;
+    int pairSum = 0;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+       Problem1 use = new Problem1(); 
+        
+       //Problem 1 Solution
+       //use.arraySize();
+       //use.setArrayData();
+       //use.sumOfPair();
+       //use.findPairsOfArray();
+       
+       System.out.println("You are now entering solution 2!");
+       
+       Problem2 use2 = new Problem2();
+       
+       //use2.makeArray();
+       //use2.sortArray();
+       
+       System.out.println("Problem THREE...THREe...THRee okay i'll stop now");
+       
+       Problem3 use3 = new Problem3();
+       
+    }//End Main Technical Problems
+        
+}//End TechnaicalProblems
